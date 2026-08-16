14a530c938b001b684590e00a40fde20e84d65ba
ashwitha <ashiwithareddy@gmail.com>
2026-08-16 16:54:52 +0530

    nested json

--- pydantic_prac.py
+++ pydantic_prac.py
@@ -11,13 +11,28 @@ class review(BaseModel):
     rating:int=Field(ge=1,le=9)
     summary:str
 
+class revresult(BaseModel):
+    reviews:list[review]
 
 
 
-r= review(
-     sentiment="positive",
-     rating=9,
-     summary="The product is excellent."
- )
+
+r = revresult(
+    reviews=[
+        {
+            "sentiment": "positive",
+            "rating": 5,
+            "summary": "The product is excellent."
+        },
+        {
+            "sentiment": "positive",
+            "rating": 4,
+            "summary": "The product is nice."
+        }
+    ]
+)
+
+
+
  
 print(r)
